16682a8161f88f0fb63d7cea73a2530e877c2143
zhangchenyu <shinome@126.com>
2023-03-07 13:29:34 +0800

    前端引入vant

--- match-frontend-ts/src/App.vue
+++ match-frontend-ts/src/App.vue
@@ -7,6 +7,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
